31010f2545991b7e0667df2e6e040b20b719a3bc
ashish379soni <ashish379soni@gmail.com>
2024-08-23 15:36:08 +0530

    first commit

---
+++ scroll_height.js
@@ -0,0 +1,3 @@
+ele = document.querySelector('body > div.x1n2onr6.xzkaem6 > div:nth-child(2) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div.xyi19xy.x1ccrb07.xtf3nb5.x1pc53ja.x1lliihq.x1iyjqo2.xs83m0k.xz65tgg.x1rife3k.x1n2onr6');
+
+return ele.scrollHeight;
---
+++ secret.py
@@ -0,0 +1,3 @@
+# Make sure that two-factor authentication is off
+username = '' # Only the instagram handle. Don't use mobile number or email id for login
+password = ''
